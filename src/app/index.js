c4cf55d117e655bc69031e749d7098977e666488
马天梦 <2541817610@qq.com>
2021-04-10 23:37:02 +0800

    :tada: init

---
+++ src/app/index.js
@@ -0,0 +1,12 @@
+/*
+ * @Description: 
+ * @Author: mTm
+ * @Date: 2021-04-10 23:33:31
+ * @LastEditTime: 2021-04-10 23:33:57
+ * @LastEditors: mTm
+ */
+const Koa = require('koa')
+
+const app = new Koa();
+
+module.exports = app
---
+++ src/main.js
@@ -0,0 +1,12 @@
+/*
+ * @Description: 
+ * @Author: mTm
+ * @Date: 2021-04-10 23:29:43
+ * @LastEditTime: 2021-04-10 23:34:29
+ * @LastEditors: mTm
+ */
+const app = require('./app');
+
+app.listen(8099, () => {
+    console.log(`项目启动成功: localhost:8099`);
+})
