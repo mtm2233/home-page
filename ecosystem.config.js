75ed846bebcee191177d92a6c8d2cd85645fe59b
MA <2541817610@qq.com>
2021-05-03 22:10:19 +0800

    chore: pm2配置

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+/*
+ * @Description:
+ * @Author: mTm
+ * @Date: 2021-05-02 22:14:02
+ * @LastEditTime: 2021-05-03 22:10:02
+ * @LastEditors: mTm
+ */
+module.exports = {
+  apps: [
+    {
+      name: "homePageApi",
+      script: "./dist/main.js",
+      watch: true,
+    },
+  ],
+};
