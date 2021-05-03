557c84b2074d1707bd2bc43f83355cc4edc70ff9
MA <2541817610@qq.com>
2021-05-03 22:12:12 +0800

    chore: pm2配置

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@
  * @Description:
  * @Author: mTm
  * @Date: 2021-05-02 22:14:02
- * @LastEditTime: 2021-05-03 22:10:02
+ * @LastEditTime: 2021-05-03 22:10:39
  * @LastEditors: mTm
  */
 module.exports = {
