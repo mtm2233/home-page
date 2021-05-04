e4b373bd8f60cce399739fd0a6fa395b92b9c1fb
MA <2541817610@qq.com>
2021-05-04 17:52:07 +0800

    fix: sso window.open => location.href

--- src/view/main/setting/components/login/Login.vue
+++ src/view/main/setting/components/login/Login.vue
@@ -2,7 +2,7 @@
  * @Description: 
  * @Author: mTm
  * @Date: 2021-05-03 22:32:37
- * @LastEditTime: 2021-05-04 17:45:56
+ * @LastEditTime: 2021-05-04 17:51:00
  * @LastEditors: mTm
 -->
 <template>
@@ -16,11 +16,10 @@ import { defineComponent } from 'vue'
 export default defineComponent({
   setup() {
     const login = () => {
-      window.open(
-        `https://sso.isdream.cn/login?url=${
-          location.origin + location.pathname
-        }`,
-      )
+      const newPath = `https://sso.isdream.cn/login?url=${
+        location.origin + location.pathname
+      }`
+      location.href = newPath
     }
 
     const save = () => {
