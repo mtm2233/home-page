3a5682483c1d9e67dd69f5a77a83867d7c818981
MA <2541817610@qq.com>
2021-05-04 23:50:23 +0800

    chore: 因less cdn有问题、改用本地

--- src/view/main/setting/components/login/Login.vue
+++ src/view/main/setting/components/login/Login.vue
@@ -2,7 +2,7 @@
  * @Description: 
  * @Author: mTm
  * @Date: 2021-05-03 22:32:37
- * @LastEditTime: 2021-05-04 17:51:00
+ * @LastEditTime: 2021-05-04 23:49:14
  * @LastEditors: mTm
 -->
 <template>
@@ -16,7 +16,7 @@ import { defineComponent } from 'vue'
 export default defineComponent({
   setup() {
     const login = () => {
-      const newPath = `https://sso.isdream.cn/login?url=${
+      const newPath = `https://sso.isdream.cn/user/login?url=${
         location.origin + location.pathname
       }`
       location.href = newPath
