48deb8d7e6acd8f6bf6fc1d4c7e4319711f7abdf
MA <2541817610@qq.com>
2021-05-17 13:51:21 +0800

    fix: 允许PATCh请求方式

--- src/app/index.ts
+++ src/app/index.ts
@@ -23,7 +23,7 @@ app.use(async (ctx: Context, next: () => Promise<any>) => {
     "Access-Control-Allow-Headers",
     "Content-Type, Content-Length, Authorization, Accept, X-Requested-With, yourHeaderFeild"
   ); //访问控制允许报头Content-Type, Content-Length, Authorization, Accept, X-Requested-With, yourHeaderFeild
-  ctx.set("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS"); //访问控制允许方法
+  ctx.set("Access-Control-Allow-Methods", "PUT, POST, GET, PATCH, DELETE, OPTIONS"); //访问控制允许方法
   ctx.set("X-Powered-By", "nodejs"); //自定义头信息，表示服务端用nodejs
   if (ctx.method == "OPTIONS") {
     ctx.body = 200; //OPTIONS类型的请求直接返回200
