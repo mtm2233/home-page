e7ac69e585f051dfde7d90763d3a53b3adad2eeb
MA <2541817610@qq.com>
2021-06-27 21:57:44 +0800

    feat: 搜索配置

--- src/interface/theme.interface.ts
+++ src/interface/theme.interface.ts
@@ -2,12 +2,14 @@
  * @Description:
  * @Author: mTm
  * @Date: 2021-05-16 22:06:22
- * @LastEditTime: 2021-05-16 22:27:18
+ * @LastEditTime: 2021-06-27 19:00:27
  * @LastEditors: mTm
  */
 interface Theme {
   primary_color?: string;
   primary_bg?: string;
+  search_website?: Boolean;
+  precise_search?: Boolean;
 }
 
 export {
--- src/service/theme.service.ts
+++ src/service/theme.service.ts
@@ -2,7 +2,7 @@
  * @Description:
  * @Author: mTm
  * @Date: 2021-05-16 21:59:43
- * @LastEditTime: 2021-05-16 22:38:56
+ * @LastEditTime: 2021-06-27 19:57:19
  * @LastEditors: mTm
  */
 import connection from "../app/database";
@@ -13,7 +13,7 @@ import { Theme } from "../interface/theme.interface";
 class ThemeService implements ServiceTheme {
   async detail(userId: number) {
     const statement = `
-        SELECT primary_color, primary_bg FROM theme WHERE user_id = ?;
+        SELECT primary_color, primary_bg, search_website, precise_search FROM theme WHERE user_id = ?;
     `;
     const [result] = await connection.execute(statement, [userId]);
     if (Array.isArray(result)) {
@@ -23,17 +23,17 @@ class ThemeService implements ServiceTheme {
   }
 
   async update(userId: number, theme: Theme) {
-    const { primary_color = null, primary_bg = null } = theme;
-    let statement = `INSERT INTO theme (user_id, primary_color, primary_bg) VALUES (?, ?, ?);`;
+    const { primary_color = null, primary_bg = null, search_website = true, precise_search = true } = theme;
+    let statement = `INSERT INTO theme (user_id, primary_color, primary_bg, search_website, precise_search) VALUES (?, ?, ?, ?, ?);`;
     const is_exist = await this.isExist(userId);
     if (is_exist) {
-      statement = `UPDATE theme SET primary_color = ?, primary_bg = ? WHERE user_id = ?;`;
+      statement = `UPDATE theme SET primary_color = ?, primary_bg = ?, search_website = ?, precise_search = ? WHERE user_id = ?;`;
     }
     const [result] = await connection.execute(
       statement,
       is_exist
-        ? [primary_color, primary_bg, userId]
-        : [userId, primary_color, primary_bg]
+        ? [primary_color, primary_bg, search_website, precise_search, userId]
+        : [userId, primary_color, primary_bg, search_website, precise_search]
     );
     return result;
   }
