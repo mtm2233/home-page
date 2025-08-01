592b4af0e3551b804926dbb6079600c8fb0cbc6f
isdream <2541817610@qq.com>
2025-08-01 23:25:59 +0800

    feat: @koa/cors

--- src/app/index.ts
+++ src/app/index.ts
@@ -7,30 +7,14 @@
  */
 import * as Koa from "koa";
 import * as bodyParser from "koa-bodyparser";
+import * as cors from "@koa/cors"
 import * as errorHandler from "./error-handler";
-
-import { Context } from "koa";
 
 import useRoutes from "../router";
 const app = new Koa();
-app.use(bodyParser());
 
-// 处理跨域
-/* 跨域设置 */
-app.use(async (ctx: Context, next: () => Promise<any>) => {
-  ctx.set("Access-Control-Allow-Origin", "*"); //访问控制允许来源：*为所有
-  ctx.set(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Content-Length, Authorization, Accept, X-Requested-With, yourHeaderFeild"
-  ); //访问控制允许报头Content-Type, Content-Length, Authorization, Accept, X-Requested-With, yourHeaderFeild
-  ctx.set("Access-Control-Allow-Methods", "PUT, POST, GET, PATCH, DELETE, OPTIONS"); //访问控制允许方法
-  ctx.set("X-Powered-By", "nodejs"); //自定义头信息，表示服务端用nodejs
-  if (ctx.method == "OPTIONS") {
-    ctx.body = 200; //OPTIONS类型的请求直接返回200
-  } else {
-    await next();
-  }
-});
+app.use(cors());
+app.use(bodyParser());
 
 useRoutes(app);
 
